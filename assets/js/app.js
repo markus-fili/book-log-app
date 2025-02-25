3eda27fd385a9bf33f40a7a767a02b4115ac551e
markus-fili <markusfili2004@gmail.com>
2025-02-25 10:26:56 -0500

    Added styles and basic index.hmtl'

--- assets/js/app.js
+++
@@ -1,4 +0,0 @@
-db.collection("test").add({ message: "Firebase is connected!" })
-.then(() => console.log("Test document added!"))
-.catch(error => console.error("Error adding document:", error));
-
